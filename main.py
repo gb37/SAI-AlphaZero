c98bf035383863e096c809e611a0d666347f29e2
gb37 <giorgio.bonito@gmail.com>
2020-09-22 20:21:55 +0200

    Cleanup

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import coloredlogs
 
 from Coach import Coach
 from othello.OthelloGame import OthelloGame as Game
-from othello.pytorch.NNet import NNetWrapper as nn
+from othello.keras.NNet import NNetWrapper as nn
 from utils import *
 
 log = logging.getLogger(__name__)
